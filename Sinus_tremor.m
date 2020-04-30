8227599de9cd043367c820b17f47fe0239ed108d
Stanema <52733202+Stanema@users.noreply.github.com>
2020-04-30 20:50:20 +0200

    Add files via upload

--- Sinus_tremor.m
+++ Sinus_tremor.m
@@ -2,17 +2,18 @@
 clc, clear all;
 
 % Video definition
-M=1500; N=1500; %matrix dimensions
+M=500; N=500; %matrix dimensions
 m = M/2; n = N/2; %center of square
 a=50; %square dimension
 fps = 60; %fps
 d = 2; %duration of video (in seconds)
-f = 0.5; % display frequency (for 60 fps - 0.1 = 1Hz; 0.5 = 5Hz; 1= 10Hz)
-r = 100; %rounding rate (recomanded value = 100)
-name='Video.avi';
+f = 5; % frequency(in Hz)
+r = M/4; %amplitude (recomanded value = axis/4)
+name="Video_"+fps+"fps_"+f+"Hz_";
 
 %% sinus definition
-t = 0:f:fps*f*d; 
+ff= f/(fps/6.3);
+t = 0:ff:fps*ff*d;
 y = sin(t);
 y = y*r;
 y = round(y);
@@ -22,15 +23,15 @@ for i = 1:length(y)
     Array{i} = zeros(M, N, 3);
     Array{i}((m-a/2)-y(i):(m+a/2)-y(i),(n-a/2):(n+a/2), :) = 1;
 end
-
+name = name+"Y_axis_tremor.avi";
 %% X axis tremor
 for i = 1:length(y)
     Array{i} = zeros(M, N, 3);
     Array{i}((m-a/2):(m+a/2),(n-a/2)-y(i):(n+a/2)-y(i), :) = 1;
 end
+name = name+"X_axis_tremor.avi";
 
 %% Video creation
-
 video = VideoWriter(name);
 video.FrameRate = fps;
 open(video);
@@ -39,7 +40,6 @@ for i=1:length(Array)
   writeVideo(video,Frame); %write the image to file
 end
 close(video);
-
 
 %% Play video
 Frames=zeros(M,N);
