98b352900f77c5f135b414711b0abc26f3b11756
Stanema <52733202+Stanema@users.noreply.github.com>
2020-05-05 19:54:30 +0200

    Add files via upload

--- Sinus_tremor.m
+++ Sinus_tremor.m
@@ -4,33 +4,60 @@ clc, clear all;
 % Video definition
 M=500; N=500; %matrix dimensions
 m = M/2; n = N/2; %center of square
-a=50; %square dimension
+a=50; %object dimension
 fps = 60; %fps
 d = 2; %duration of video (in seconds)
 f = 5; % frequency(in Hz)
-r = M/4; %amplitude (recomanded value = axis/4)
+r = M/4; %amplitude (default value = axis/4)
+noise = 0.001; %noise (0 = no noise, default noise = 0.001)
+noise_type = 'gaussian'; % ('salt & pepper' or 'gaussian')
 name="Video_"+fps+"fps_"+f+"Hz_";
 
 %% sinus definition
 ff= f/(fps/(2*pi));
 t = 0:ff:fps*ff*d;
-y = sin(t);
-y = y*r;
-y = round(y);
+sinus = sin(t);
+sinus = sinus*r;
+sinus = round(sinus);
 
-%% Y axis tremor
-for i = 1:length(y)
+%% Y axis square tremor
+for i = 1:length(sinus)
     Array{i} = zeros(M, N, 3);
-    Array{i}((m-a/2)-y(i):(m+a/2)-y(i),(n-a/2):(n+a/2), :) = 1;
+    Array{i} = imnoise(Array{i},noise_type,noise);
+    Array{i}((m-a/2)-sinus(i):(m+a/2)-sinus(i),(n-a/2):(n+a/2), :) = 1;
 end
-name = name+"Y_axis_tremor.avi";
-%% X axis tremor
-for i = 1:length(y)
+name = name+"Y_axis_square_tremor.avi";
+%% X axis square tremor
+for i = 1:length(sinus)
     Array{i} = zeros(M, N, 3);
-    Array{i}((m-a/2):(m+a/2),(n-a/2)-y(i):(n+a/2)-y(i), :) = 1;
+    Array{i} = imnoise(Array{i},noise_type,noise);
+    Array{i}((m-a/2):(m+a/2),(n-a/2)-sinus(i):(n+a/2)-sinus(i), :) = 1;
 end
-name = name+"X_axis_tremor.avi";
-
+name = name+"X_axis_square_tremor.avi";
+%% Y axis circle tremor
+x=(1:M).';
+y=1:N;
+for i = 1:length(sinus)
+    r=a/2;
+    A = zeros(M, N, 3);
+    A=(x-(m - sinus(i))).^2 + (y-n).^2 <= r^2; 
+    A = 255 * repmat(uint8(A), 1, 1, 3);
+    Array{i} = A;
+    Array{i} = imnoise(Array{i},noise_type,noise);
+end
+name = name+"Y_axis_circle_tremor.avi";
+%% X axis circle tremor
+x=(1:M).';
+y=1:N;
+for i = 1:length(sinus)
+    r=a/2;
+    A = zeros(M, N, 3);
+    A=(x-m).^2 + (y-(n - sinus(i))).^2 <= r^2;  
+    A = 255 * repmat(uint8(A), 1, 1, 3);
+    Array{i} = A;
+    Array{i} = imnoise(Array{i},noise_type,noise);
+end
+name = name+"X_axis_circle_tremor.avi";
 %% Video creation
 video = VideoWriter(name);
 video.FrameRate = fps;
@@ -47,4 +74,4 @@ Frames=zeros(M,N);
 for i=1:length(Array)
   Frames(:,:,i) = rgb2gray(Array{1,i});
 end
-implay(Frames);
+implay(Frames, fps);
