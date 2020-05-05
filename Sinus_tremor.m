ea358b3702451454cc14c5b884757e195b7c4b45
Stanema <52733202+Stanema@users.noreply.github.com>
2020-05-05 20:09:55 +0200

    Add files via upload

--- Sinus_tremor.m
+++ Sinus_tremor.m
@@ -3,12 +3,12 @@ clc, clear all;
 
 % Video definition
 M=500; N=500; %matrix dimensions
-m = M/2; n = N/2; %center of square
+m = 100; n = 100; %center of square (default values = m = M/2, n = N/2)
 a=50; %object dimension
 fps = 60; %fps
 d = 2; %duration of video (in seconds)
 f = 5; % frequency(in Hz)
-r = M/4; %amplitude (default value = axis/4)
+amp = M/4; %amplitude (default value = axis/4)
 noise = 0.001; %noise (0 = no noise, default noise = 0.001)
 noise_type = 'gaussian'; % ('salt & pepper' or 'gaussian')
 name="Video_"+fps+"fps_"+f+"Hz_";
@@ -17,24 +17,39 @@ name="Video_"+fps+"fps_"+f+"Hz_";
 ff= f/(fps/(2*pi));
 t = 0:ff:fps*ff*d;
 sinus = sin(t);
-sinus = sinus*r;
+sinus = sinus*amp;
 sinus = round(sinus);
 
 %% Y axis square tremor
-for i = 1:length(sinus)
-    Array{i} = zeros(M, N, 3);
-    Array{i} = imnoise(Array{i},noise_type,noise);
-    Array{i}((m-a/2)-sinus(i):(m+a/2)-sinus(i),(n-a/2):(n+a/2), :) = 1;
+try
+    for i = 1:length(sinus)
+        Array{i} = zeros(M, N, 3);
+        Array{i} = imnoise(Array{i},noise_type,noise);
+        Array{i}((m-a/2)-sinus(i):(m+a/2)-sinus(i),(n-a/2):(n+a/2), :) = 1;
+    end
+    name = name+"Y_axis_square_tremor.avi";
+catch
+    warning('Problem using function.  Setting to default values');
+    m = M/2; n = N/2; %center of square (default values = m = M/2, n = N/2)
+    a=50; %object dimension
+    amp = M/4;
 end
-name = name+"Y_axis_square_tremor.avi";
 %% X axis square tremor
+try
 for i = 1:length(sinus)
     Array{i} = zeros(M, N, 3);
     Array{i} = imnoise(Array{i},noise_type,noise);
     Array{i}((m-a/2):(m+a/2),(n-a/2)-sinus(i):(n+a/2)-sinus(i), :) = 1;
 end
 name = name+"X_axis_square_tremor.avi";
+catch
+    warning('Problem using function.  Setting to default values');
+    m = M/2; n = N/2; %center of square (default values = m = M/2, n = N/2)
+    a=50; %object dimension
+    amp = M/4;
+end
 %% Y axis circle tremor
+try
 x=(1:M).';
 y=1:N;
 for i = 1:length(sinus)
@@ -46,7 +61,14 @@ for i = 1:length(sinus)
     Array{i} = imnoise(Array{i},noise_type,noise);
 end
 name = name+"Y_axis_circle_tremor.avi";
+catch
+    warning('Problem using function.  Setting to default values');
+    m = M/2; n = N/2; %center of square (default values = m = M/2, n = N/2)
+    a=50; %object dimension
+    amp = M/4;
+end
 %% X axis circle tremor
+try
 x=(1:M).';
 y=1:N;
 for i = 1:length(sinus)
@@ -58,6 +80,12 @@ for i = 1:length(sinus)
     Array{i} = imnoise(Array{i},noise_type,noise);
 end
 name = name+"X_axis_circle_tremor.avi";
+catch
+    warning('Problem using function.  Setting to default values');
+    m = M/2; n = N/2; %center of square (default values = m = M/2, n = N/2)
+    a=50; %object dimension
+    amp = M/4;
+end
 %% Video creation
 video = VideoWriter(name);
 video.FrameRate = fps;
